ffef456f575ffe0423f75437ef7bddfe76dfc63c
italofelipe <felipe.italo@hotmail.com>
2019-09-01 16:24:22 -0300

    Fim da aula 50: Feito o v-bind entre o App.vue e o componente VideoList, adicionado o Data no App.vue, que é equivalente ao State do React.

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,30 @@
   <div class="wrapper">
     <h3>Hello Vue!</h3>
     <SearchBar @termChange="onTermChange"></SearchBar>
+
+    <VideoList v-bind:videos="videos" />
+    <!-- <VideoList :videos="videos" />  - Essa Sintaxe sem o v-bind, significa exatamente a mesma coisa que a linha de cima.-->
+    <span class="counter">{{videos.length}} videos matching this term</span>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 const API_KEY = "AIzaSyDTAQKEaJr-ww5OctDk7MuCaAR6jkHdYfM";
 export default {
   name: "App",
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return { videos: [], term: "" };
   },
   methods: {
     onTermChange(searchTerm) {
+      this.term = searchTerm;
       axios
         .get("https://www.googleapis.com/youtube/v3/search", {
           params: {
@@ -26,9 +36,18 @@ export default {
           }
         })
         .then(res => {
-          console.log(res);
+          // Aqui estou fazendo o equivalente ao this.setState()
+          this.videos = res.data.items;
+          console.log(`State do Vue: ${this.term}`);
         });
     }
   }
 };
 </script>
+
+<style scoped>
+.counter {
+  font-weight: bold;
+  font-size: 1em;
+}
+</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      <li>Video 1</li>
+      {{videos}}
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoList"
+};
+</script>
+
+<style>
+</style>
