fad9298766f56e6d6eafdd8ec91cafa5f495af40
italofelipe <felipe.italo@hotmail.com>
2019-08-31 21:25:49 -0300

    Aula 45 - Adicionada a API do Youtube ao código, mas ainda nao traz nada visualmente.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search-bar-container">
+    <input type="text" class="video-input" @input="onInput" placeholder="You can search anything!" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput(e) {
+      this.$emit("termChange", e.target.value);
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Consolas, sans-serif;
+}
+.search-bar-container {
+  border: 1px solid #ccc;
+  padding: 10px 20px;
+  box-shadow: 3px 3px 3px #ababab;
+  width: 40vw;
+}
+.video-input {
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #999;
+  padding: 10px 5px;
+}
+</style>
